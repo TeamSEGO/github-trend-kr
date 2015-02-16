2cd8aaf89f112c8738db948e51cc227a549481f8
김광진 <kjink.kim@samsung.com>
2015-02-16 13:37:44 +0900

    internet explorer compatibility and modify fb social comment uri

--- js/Segobook.js
+++ js/Segobook.js
@@ -2,13 +2,21 @@ var app = angular.module('segoApp', ['ngRoute']);
 //var contentapiurl = 'https://api.github.com/repos/zeroxy/github-trend-kr/contents/';
 //var postfix = "?ref=gh-pages&callback=JSON_CALLBACK";
 
+var footer_template = function(hashuri){
+  return '<footer class="text-center"><div class="footer-below"><div class="container"><div class="row"><div class="col-lg-12">'+
+'<div class="fb-comments" data-href="http://teamsego.github.io/github-trend-kr/#"'+hashuri+' data-width="100%" data-numposts="5" data-colorscheme="dark"></div>'+
+'</div></div></div></div></footer>';
+}
+var section_template = function(){
+  return '<section ng-repeat="md in mddata track by $index" ng-class-even="\'success\'"><div class="container"><div class="row"><div class="col-lg-8 col-lg-offset-2"><h1>{{$index+1}}</h1><markdown renderdata = "md"></markdown></div></div></div></section>';
+}
 app.filter('nonamed', function(){
   return function(input){return (input == undefined || input == null)?'no-named':input;}
 });
 
 app.config(function($routeProvider) {
     $routeProvider
-    .when('/:vol', {template:'<section ng-repeat="md in mddata track by $index" ng-class-even="\'success\'"><div class="container"><div class="row"><div class="col-lg-8 col-lg-offset-2"><h1>{{$index+1}}</h1><markdown renderdata = "md"></markdown></div></div></div></section>', controller:'articleCtrl'})
+    .when('/:vol', {template:section_template()+footer_template(params.vol), controller:'articleCtrl'})
     //.when('/', {templateUrl:'/README.md', controller:'articleCtrl'})
     .otherwise({ redirectTo: '/'+menus[menus.length-1].volume.name});
 });
@@ -41,8 +49,7 @@ app.controller('articleCtrl',function ($scope,$http, $routeParams){
 
 });
 
-app.controller('segoCtrl',function($scope, $route, $routeParams){
-  console.log($routeParams);
+app.controller('segoCtrl',function($scope){
   $scope.menus = menus;
 });
 
