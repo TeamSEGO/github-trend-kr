cb079902513c27b057ce6e0ec8d6ca99dba4082b
keen <ehrudxo@hotmail.com>
2015-02-20 23:02:57 +0900

    index adjust

--- js/Segobook.js
+++ js/Segobook.js
@@ -16,7 +16,7 @@ app.filter('nonamed', function(){
 
 app.config(function($routeProvider) {
     $routeProvider
-    .when('/:vol', {template:'<section ng-repeat="md in mddata track by $index" ng-class-even="\'success\'"><div class="container"><div class="row"><div class="col-lg-8 col-lg-offset-2"><h1>{{$index+1}}</h1><markdown renderdata = "md"></markdown></div></div></div></section>', controller:'articleCtrl'})
+    .when('/:vol', {template:'<section ng-repeat="md in mddata track by $index" ng-class-even="\'success\'"><div class="container"><div class="row"><div class="col-lg-8 col-lg-offset-2"><h1>{{$index}}</h1><markdown renderdata = "md"></markdown></div></div></div></section>', controller:'articleCtrl'})
     //.when('/', {templateUrl:'/README.md', controller:'articleCtrl'})
     .otherwise({ redirectTo: '/'+menus[menus.length-1].volume.name});
 });
